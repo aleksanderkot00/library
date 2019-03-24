31f9a83f6da6b8935b437493af36dfdb56697366
unknown <aleksanderkot00@gmail.com>
2019-03-24 20:35:40 +0100

    Another change HttpStatus in WrongBookStatusException

--- src/main/java/com/crud/library/exception/WrongBookStatusException.java
+++ src/main/java/com/crud/library/exception/WrongBookStatusException.java
@@ -3,6 +3,6 @@ package com.crud.library.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE)
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
 public class WrongBookStatusException extends RuntimeException{
 }
